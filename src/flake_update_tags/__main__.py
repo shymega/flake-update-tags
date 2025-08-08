0fdf5610b2e9fe890f805aa42709d502f203fffc
Dom Rodriguez <shymega@shymega.org.uk>
2025-08-08 07:58:17 +0100

    feat: Initial commit
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ src/flake_update_tags/__main__.py
@@ -0,0 +1,19 @@
+import json
+import argparse
+import logging
+import os
+import pathlib
+import subprocess
+from typing import Self
+
+class Flake:
+    def __init__(self, path: pathlib.PurePosixPath) -> Self:
+        self.__path: pathlib.PurePosixPath = path
+
+        return self
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
