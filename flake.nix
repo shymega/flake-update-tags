shymega/flake-follows-auto

feat: Initial commit
Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

flake.nix
@@ -1,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+  outputs = inputs: let
+    inherit (inputs) self nixpkgs;
+    forEachSystem = let
+      inherit (nixpkgs.lib) genAttrs;
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+      ];
+    in
+      f: genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+  in {
+    packages =
+      forEachSystem
+      (pkgs: {
+        flake-follows-auto = pkgs.callPackage ./package.nix {inherit self;};
+        default = self.packages.${pkgs.system}.flake-follows-auto;
+      });
+  };
+}

package.nix
@@ -1,0 +1,24 @@
+{
+  lib,
+  python3Packages,
+  python3,
+  self,
+}: let
+  inherit (python3Packages) buildPythonApplication;
+in
+  buildPythonApplication {
+    pname = "flake-follows-auto";
+    version = "0.1.0";
+    pyproject = true;
+    src = self;
+
+    build-system = with python3Packages; [
+      pdm-backend
+    ];
+
+    meta = {
+      maintainer = with lib.maintainers; [shymega];
+      mainProgram = "flake-follows-auto";
+      platforms = lib.platforms.unix;
+    };
+  }

shymega/flake-update-tags

flake.nix
@@ -1,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+  outputs = inputs: let
+    inherit (inputs) self nixpkgs;
+    forEachSystem = let
+      inherit (nixpkgs.lib) genAttrs;
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+      ];
+    in
+      f: genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+  in {
+    packages =
+      forEachSystem
+      (pkgs: {
+        flake-update-tags = pkgs.callPackage ./package.nix {inherit self;};
+        default = self.packages.${pkgs.stdenv.hostPlatform.system}.flake-update-tags;
+      });
+  };
+}

package.nix
@@ -1,0 +1,24 @@
+{
+  lib,
+  python3Packages,
+  python3,
+  self,
+}: let
+  inherit (python3Packages) buildPythonApplication;
+in
+  buildPythonApplication {
+    pname = "flake-update-tags";
+    version = "0.1.0";
+    pyproject = true;
+    src = self;
+
+    build-system = with python3Packages; [
+      pdm-backend
+    ];
+
+    meta = {
+      maintainer = with lib.maintainers; [shymega];
+      mainProgram = "flake-update-tags";
+      platforms = lib.platforms.unix;
+    };
+  }